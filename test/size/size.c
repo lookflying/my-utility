36f77e21f7defa1f036fdf8848e4231c2183c9f2
lookflying <lookflying@gmail.com>
2014-07-07 22:16:23 +0800

    add print:

--- test/size/size.c
+++ test/size/size.c
@@ -3,6 +3,8 @@
 int main(int argc, char* argv[])
 {
 	printf("size of unsigned long %lu\n", sizeof(unsigned long));
+	printf("largest unsigned long = %lu\n", (unsigned long)((long)0 - 1));
 	printf("size of unsigned long long %lu\n", sizeof(unsigned long long));
+	printf("largest unsigned long long  = %llu\n", (unsigned long long)((long long)0 - 1));
 	return 0;
 }
