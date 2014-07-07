53ecae7865d439615fe8df0fd2b93c8b47d76232
lookflying <lookflying@gmail.com>
2014-07-07 22:11:34 +0800

    add long long

--- test/size/size.c
+++ test/size/size.c
@@ -3,5 +3,6 @@
 int main(int argc, char* argv[])
 {
 	printf("size of unsigned long %lu\n", sizeof(unsigned long));
+	printf("size of unsigned long long %lu\n", sizeof(unsigned long long));
 	return 0;
 }
