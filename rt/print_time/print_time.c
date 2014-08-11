e885824634dc5dc0c70069717e381065b5a20f93
lookflying <lookflying@gmail.com>
2014-08-12 00:17:22 +0800

    print_time print ms now

--- rt/print_time/print_time.c
+++ rt/print_time/print_time.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
-unsigned long long ts_to_ull(struct timespec *t)
+unsigned long long ts_to_ns_ull(struct timespec *t)
 {
-	return (unsigned long long)t->tv_sec * 1E9 + (unsigned long long)t->tv_nsec / 1000;
+	return (unsigned long long)t->tv_sec * 1E9 + (unsigned long long)t->tv_nsec ;
 }
+
+unsigned long long ts_to_ms_ull(struct timespec *t)
+{
+	return (unsigned long long)t->tv_sec * 1E6 + (unsigned long long)t->tv_nsec/1000 ;
+}
+
 
 void print_cur_ts(void)
 {
 	struct timespec t;
 	unsigned long long t_ull;
 	clock_gettime(CLOCK_MONOTONIC, &t);
-	printf("ts = %llu\n", ts_to_ull(&t));
+	printf("ts = %llu\n", ts_to_ms_ull(&t));
 }
 
 int main(int argc, char* argv[])
