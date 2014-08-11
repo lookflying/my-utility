54e9d28fed953c6b52ce102af5d14f7b41c8e912
root <root@kvm.(none)>
2014-08-12 00:31:53 +0800

    fix to show us

--- rt/print_time/print_time.c
+++ rt/print_time/print_time.c
@@ -6,7 +6,7 @@ unsigned long long ts_to_ns_ull(struct timespec *t)
 	return (unsigned long long)t->tv_sec * 1E9 + (unsigned long long)t->tv_nsec ;
 }
 
-unsigned long long ts_to_ms_ull(struct timespec *t)
+unsigned long long ts_to_us_ull(struct timespec *t)
 {
 	return (unsigned long long)t->tv_sec * 1E6 + (unsigned long long)t->tv_nsec/1000 ;
 }
@@ -17,7 +17,7 @@ void print_cur_ts(void)
 	struct timespec t;
 	unsigned long long t_ull;
 	clock_gettime(CLOCK_MONOTONIC, &t);
-	printf("ts = %llu\n", ts_to_ms_ull(&t));
+	printf("ts = %llu\n", ts_to_us_ull(&t));
 }
 
 int main(int argc, char* argv[])
